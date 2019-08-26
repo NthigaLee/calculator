0d2e979c1ffb4d5bed45f9ef897d5555b6dcbb25
NthigaLee <nthigalee@gmail.com>
2019-08-26 16:37:16 +0300

    Calculator first commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+var calc=function(number1,number2){
+    number1=parseInt(prompt("Enter a number:"));
+    number2=parseInt(prompt("Enter another one:"));
+    number1;
+    number2;
+    var answer=number1+number2;
+    alert(answer);
+}
+calc();
